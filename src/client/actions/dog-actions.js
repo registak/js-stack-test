c0907868ae2f432b12cbe2a14acc89785bbebea5
registak <prkaz4m@gmail.com>
2016-12-02 15:41:57 +0900

    add test module mocha and chai

--- src/client/actions/dog-actions.js
+++ src/client/actions/dog-actions.js
@@ -1,4 +1,4 @@
-import {createAction} from 'redux-actions';
+import { createAction } from 'redux-actions';
 
 export const MAKE_BARK = 'MAKE_BARK';
 
--- src/shared/dog.js
+++ src/shared/dog.js
@@ -6,6 +6,14 @@ class Dog {
   bark() {
     return `Wah wah, I am ${this.name}`;
   }
+
+  barkInConsole() {
+    /* for test */
+    /* eslint-disable no-console */
+    console.log(this.bark());
+    /* eslint-enable no-console */
+  }
+
 }
 
 export default Dog;
